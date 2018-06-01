2aaecf3b9e42190e967d78cd2e86572592feb37a
Taesu Hyeon <qvil1127@gmail.com>
2018-06-02 00:37:29 +0900

    Add Image type, gh-pages

--- frontend/src/components/Header/Logo.tsx
+++ frontend/src/components/Header/Logo.tsx
@@ -1,10 +1,24 @@
+import * as React from "react";
 import styled from "styled-components";
 
-const Logo = styled.img`
+const Img = styled<ILogoProps, any>("img")`
   width: 160px;
   height: 26px;
   margin-left: 30px;
   src: ${props => props.src};
 `;
+
+interface ILogoProps {
+  src?: HTMLImageElement & string & File;
+}
+
+const Logo: React.SFC<ILogoProps> = props => {
+  return (
+    <a href="https://qvil.github.io/cashnote-clone/">
+      <Img {...props} />
+      {/* <Img src={props.src} /> */}
+    </a>
+  );
+};
 
 export default Logo;
